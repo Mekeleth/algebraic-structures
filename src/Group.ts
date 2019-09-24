d802595da2f943dc6d77085bd21ffa6fd7e0a948
Mateusz Piątkowski <garadim12@gmail.com>
2019-09-24 16:47:04 +0200

    added some conditions in the Group class constructor, added some options in tsconfig

--- src/Group.ts
+++ src/Group.ts
@@ -1,19 +1,40 @@
 class Group<T> {
-  constructor(public add: (x: T, y: T) => T, public neutral: T){
-    // if(this.add(1, this.neutral) !== 1 || this.add(this.neutral, 1) !== this.neutral) throw new Error('The addition operation does not respect the law of neutral element.')
+  constructor(public set: T[], public neutral: T, public add: (x: T, y: T) => T, public inverse: (x: T) => T) {
+    let setCopy: T[] = set
+    let subArray: T[] = new Array(Math.ceil(Math.sqrt(setCopy.length)))
+    let a: T, b: T, inverted: T
+    for (let i = 0; i < subArray.length; ++i) {
+      subArray[i] = setCopy.splice(Math.floor(Math.random() * setCopy.length), 1)[0]
+
+      // if (!set.includes(<number>add(subArray[i], Math.floor(Math.random() * set.length)))) throw new Error('The operation is not internal.')
+
+      inverted = add(neutral, inverse(subArray[i]))
+      console.log(subArray[i])
+      console.log({ inverted })
+      if (!Object.is(add(subArray[i], inverted), neutral) || !Object.is(add(inverted, subArray[i]), neutral)) {
+        throw new Error('Inverse function is not harmonized correctly with addition operation.')
+      }
+
+      if (add(subArray[i], neutral) !== subArray[i] || add(neutral, subArray[i]) !== subArray[i]) throw new Error('Either given element is not neutral or the operation is incorrectly defined.')
+
+      for (let j = 0; j < Math.ceil(Math.sqrt(set.length)); ++j) {
+        a = set[Math.floor(Math.random() * set.length)]
+        b = set[Math.floor(Math.random() * set.length)]
+
+        if (add(add(subArray[i], a), b) !== add(subArray[i], add(a, b)) && !Object.is(add(add(subArray[i], a), b), add(subArray[i], add(a, b)))) throw new Error('The operation is not associative.')
+      }
+    }
   }
 
 }
 
 class AbelianGroup<T> extends Group<T> {
-  constructor(public add: (x: T, y: T) => T, public neutral: T) {
-    super(add, neutral)
-    // let x: T = <T>(new Number(Math.random()*100))
-    // let y: T = <T>(new Number(Math.random()*100))
-    // console.log(add(x,y))
-    // if(add(x, y) !== add(y, x)) throw new Error('This group is not abelian.')
+  constructor(public set: T[], public neutral: T, public add: (x: T, y: T) => T, public inverse: (x: T) => T) {
+    super(set, neutral, add, inverse)
   }
 }
+
+console.log(typeof Symbol('ss'))
 
 export { Group, AbelianGroup }
 export default AbelianGroup
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
 import { Group, AbelianGroup } from './Group'
 
-let x = new AbelianGroup<number>((x:number,y:number) => 2*x+y, 0)
-console.log(x.add(3,x.neutral))
+let x = new AbelianGroup<number>([...Array(100).keys()], 98, (x: number, y: number) => (x + y + 2) % 100, (x: number) => 98 - (x + 2) % 100)
